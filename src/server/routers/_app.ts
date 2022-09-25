027a7d2bc2b0c166aed2d3b0aa0fb04112a811b4
vs222qj <vs222qj@student.lnu.se>
2022-09-25 19:28:40 +0200

    add likes router

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -2,6 +2,7 @@ import { Context } from '../context'
 import * as trpc from '@trpc/server'
 import { posts } from './posts'
 import { users } from './users'
+import { likes } from './likes'
 
 const createRouter = () => {
   return trpc.router<Context>()
@@ -10,5 +11,6 @@ const createRouter = () => {
 export const appRouter = createRouter()
   .merge('post.', posts)
   .merge('user.', users)
+  .merge('like.', likes)
 
 export type AppRouter = typeof appRouter
---
+++ src/server/routers/likes.ts
@@ -0,0 +1,37 @@
+import { z } from 'zod'
+import { createRouter } from '../createRouter'
+import { prisma } from '../prisma'
+
+export const likes = createRouter()
+  .mutation('add', {
+    input: z.object({
+      postId: z.number()
+    }),
+    async resolve({ input, ctx }) {
+      const { postId } = input
+
+      const result = await prisma.forum_post_likes.findFirst({
+        where: {
+          user_id: parseInt(ctx.user!.id),
+          post_id: postId
+        }
+      })
+
+      if (!result) {
+        try {
+          await prisma.forum_post_likes.create({
+            data: {
+              post_id: postId,
+              user_id: parseInt(ctx.user!.id)
+            }
+          })
+
+          return true
+        } catch (e) {
+          return false
+        }
+      } else {
+        return false
+      }
+    }
+  })
