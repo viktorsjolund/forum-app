d444568763792460308e22ac698fe8e3effe0496
vs222qj <vs222qj@student.lnu.se>
2022-09-25 21:04:26 +0200

    add dislikes router

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -3,6 +3,7 @@ import * as trpc from '@trpc/server'
 import { posts } from './posts'
 import { users } from './users'
 import { likes } from './likes'
+import { dislikes } from './dislikes'
 
 const createRouter = () => {
   return trpc.router<Context>()
@@ -12,5 +13,6 @@ export const appRouter = createRouter()
   .merge('post.', posts)
   .merge('user.', users)
   .merge('like.', likes)
+  .merge('dislike.', dislikes)
 
 export type AppRouter = typeof appRouter
---
+++ src/server/routers/dislikes.ts
@@ -0,0 +1,88 @@
+import { z } from 'zod'
+import { createRouter } from '../createRouter'
+import { prisma } from '../prisma'
+import * as trpc from '@trpc/server'
+
+export const dislikes = createRouter()
+  .mutation('add', {
+    input: z.object({
+      postId: z.number()
+    }),
+    async resolve({ input, ctx }) {
+      const { postId } = input
+
+      const result = await prisma.forum_post_dislikes.findFirst({
+        where: {
+          user_id: parseInt(ctx.user!.id),
+          post_id: postId
+        }
+      })
+
+      if (!result) {
+        try {
+          await prisma.forum_post_dislikes.create({
+            data: {
+              post_id: postId,
+              user_id: parseInt(ctx.user!.id)
+            }
+          })
+
+          return true
+        } catch (e) {
+          throw new trpc.TRPCError({
+            code: 'INTERNAL_SERVER_ERROR',
+            message: 'Something went wrong...'
+          })
+        }
+      } else {
+        return false
+      }
+    }
+  })
+  .mutation('remove', {
+    input: z.object({
+      postId: z.number()
+    }),
+    async resolve ({ input, ctx }) {
+      const { postId } = input
+
+      try {
+        await prisma.forum_post_dislikes.deleteMany({
+          where: {
+            user_id: parseInt(ctx.user!.id),
+            post_id: postId,
+          }
+        })
+
+        return true
+      } catch (e) {
+        throw new trpc.TRPCError({
+          code: 'INTERNAL_SERVER_ERROR',
+          message: 'Something went wrong...'
+        })
+      }
+    }
+  })
+  .query('totalCountByPostId', {
+    input: z.object({
+      postId: z.number()
+    }),
+    async resolve({ input }) {
+      const { postId } = input
+
+      try {
+        const result = await prisma.forum_post_dislikes.findMany({
+          where: {
+            post_id: postId
+          }
+        })
+
+        return result.length
+      } catch (e) {
+        throw new trpc.TRPCError({
+          code: 'INTERNAL_SERVER_ERROR',
+          message: 'Something went wrong...'
+        })
+      }
+    }
+  })
