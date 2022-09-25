cec0ef38596959ec49c2d1c6eeb4eac2a1bb201d
vs222qj <vs222qj@student.lnu.se>
2022-09-26 00:13:19 +0200

    add likes and dislikes to post

--- src/pages/view-post/[pid].tsx
+++ src/pages/view-post/[pid].tsx
@@ -1,14 +1,20 @@
 import Header from '@/components/header'
 import { trpc } from '@/utils/trpc'
-import { Typography } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import { useRouter } from 'next/router'
+import { ThumbUpOffAlt, ThumbDownOffAlt } from '@mui/icons-material'
 
 const Post = () => {
   const router = useRouter()
   const { pid } = router.query
   if (!pid) return
 
-  const post = trpc.useQuery(['post.byId', { id: Array.isArray(pid) ? parseInt(pid[0]) : parseInt(pid) }])
+  const pidParsed = Array.isArray(pid) ? parseInt(pid[0]) : parseInt(pid)
+
+  const likes = trpc.useQuery(['like.totalCountByPostId', { postId: pidParsed }])
+  const dislikes = trpc.useQuery(['dislike.totalCountByPostId', { postId: pidParsed }])
+  const post = trpc.useQuery(['post.byId', { id: pidParsed }])
+
   if (!post.data) {
     return <div>Loading...</div>
   }
@@ -39,6 +45,20 @@ const Post = () => {
       <Typography color='white'>
         {post.data.author.username}
       </Typography>
+      <Box display='flex' sx={{ svg: { cursor: 'pointer' }}}>
+        <Box display='flex'>
+          <Typography> 
+            {likes.data}
+          </Typography>
+          <ThumbUpOffAlt />
+        </Box>
+        <Box display='flex'>
+          <ThumbDownOffAlt />
+          <Typography> 
+            {dislikes.data}
+          </Typography>
+        </Box>
+      </Box>
     </div>
   )
 }
