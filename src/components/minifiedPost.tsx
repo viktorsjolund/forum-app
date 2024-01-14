ed966b83bd4a11e4c08e2743f8963e3d0ec7f19f
vs222qj <vs222qj@student.lnu.se>
2024-01-14 02:35:38 +0100

    add <a> tag to link

--- src/components/minifiedPost.tsx
+++ src/components/minifiedPost.tsx
@@ -23,46 +23,44 @@ export const MinifiedPost = (props: TMinifiedPostProps) => {
       href={`/view-post/${post.id}`}
       passHref
     >
-      <div className='flex h-20 bg-midnight-dark border-[1px] border-slate-700 rounded p-2 w-full cursor-pointer hover:border-main-purple-light transition-colors'>
-        <div className='flex flex-col h-full justify-center border-r-2 border-slate-800 pr-2 w-fit min-w-[5rem]'>
-          <div className='flex items-center mb-1'>
-            <AiFillLike />
-            <span className='pl-2 text-gray-300 text-sm'>{post._count.likes}</span>
+      <a>
+        <div className='flex h-20 bg-midnight-dark border-[1px] border-slate-700 rounded p-2 w-full cursor-pointer hover:border-main-purple-light transition-colors'>
+          <div className='flex flex-col h-full justify-center border-r-2 border-slate-800 pr-2 w-fit min-w-[5rem]'>
+            <div className='flex items-center mb-1'>
+              <AiFillLike />
+              <span className='pl-2 text-gray-300 text-sm'>{post._count.likes}</span>
+            </div>
+            <div className='flex items-center mb-1'>
+              <AiFillDislike />
+              <span className='pl-2 text-gray-300 text-sm'>{post._count.dislikes}</span>
+            </div>
+            <div className='flex items-center'>
+              <FaCommentDots />
+              <span className='pl-2 text-gray-300 text-sm'>{post._count.comments}</span>
+            </div>
           </div>
-          <div className='flex items-center mb-1'>
-            <AiFillDislike />
-            <span className='pl-2 text-gray-300 text-sm'>{post._count.dislikes}</span>
+          <div className='pl-4 flex flex-col w-3/4'>
+            <span className='font-extrabold text-[1em] leading-5 h-fit break-words'>
+              {post.title}
+            </span>
           </div>
-          <div className='flex items-center'>
-            <FaCommentDots />
-            <span className='pl-2 text-gray-300 text-sm'>{post._count.comments}</span>
-          </div>
-        </div>
-        <div className='pl-4 flex flex-col w-3/4'>
-          <span className='font-extrabold text-[1em] leading-5 h-fit break-words'>
-            {post.title}
-          </span>
-        </div>
-        <div className='ml-auto flex flex-col'>
-          <span className='text-sm h-fit ml-auto'>
-            by
-            <Link
-              href={`/account/${post.author.username}`}
-              passHref
-            >
-              <span className='font-bold underline pl-1 hover:text-main-purple-light transition-colors'>
+          <div className='ml-auto flex flex-col'>
+            <span className='text-sm h-fit ml-auto'>
+              <span>by</span>
+              <span className='font-bold underline pl-1 transition-colors'>
                 {post.author.username}
               </span>
-            </Link>
-          </span>
-          <span className='text-xs h-fit mt-auto ml-auto text-gray-500'>
-            Posted on: {moment(post.created_at.toString(), 'YYYY-MM-DD HH:mm:ss').format('lll')}
-          </span>
-          <span className='text-xs h-fit mt-auto text-gray-500'>
-            Last edited: {moment(post.updated_at?.toString(), 'YYYY-MM-DD HH:mm:ss').format('lll')}
-          </span>
+            </span>
+            <span className='text-xs h-fit mt-auto ml-auto text-gray-500'>
+              Posted on: {moment(post.created_at.toString(), 'YYYY-MM-DD HH:mm:ss').format('lll')}
+            </span>
+            <span className='text-xs h-fit mt-auto text-gray-500'>
+              Last edited:{' '}
+              {moment(post.updated_at?.toString(), 'YYYY-MM-DD HH:mm:ss').format('lll')}
+            </span>
+          </div>
         </div>
-      </div>
+      </a>
     </Link>
   )
 }
