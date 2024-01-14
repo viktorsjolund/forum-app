c98331999d7153a9cdea6953b9a8da29f424e160
vs222qj <vs222qj@student.lnu.se>
2024-01-14 03:02:34 +0100

    add usepostrating to minified post

--- src/components/minifiedPost.tsx
+++ src/components/minifiedPost.tsx
@@ -1,8 +1,9 @@
-import { AiFillDislike, AiFillLike } from 'react-icons/ai'
+import { AiFillDislike, AiFillLike, AiOutlineLike, AiOutlineDislike } from 'react-icons/ai'
 import { FaCommentDots } from 'react-icons/fa'
 import type { post, user } from '@prisma/client'
 import Link from 'next/link'
 import moment from 'moment'
+import { usePostRating } from '@/hooks/usePostRating'
 
 type TMinifiedPostProps = {
   post: post & {
@@ -17,50 +18,65 @@ type TMinifiedPostProps = {
 
 export const MinifiedPost = (props: TMinifiedPostProps) => {
   const { post } = props
+  const [handleLike, handleDislike, isLiked, isDisliked] = usePostRating(post.id)
 
   return (
-    <Link
-      href={`/view-post/${post.id}`}
-      passHref
-    >
-      <a>
-        <div className='flex h-20 bg-midnight-dark border-[1px] border-slate-700 rounded p-2 w-full cursor-pointer hover:border-main-purple-light transition-colors'>
-          <div className='flex flex-col h-full justify-center border-r-2 border-slate-800 pr-2 w-fit min-w-[5rem]'>
-            <div className='flex items-center mb-1'>
-              <AiFillLike />
-              <span className='pl-2 text-gray-300 text-sm'>{post._count.likes}</span>
-            </div>
-            <div className='flex items-center mb-1'>
-              <AiFillDislike />
-              <span className='pl-2 text-gray-300 text-sm'>{post._count.dislikes}</span>
-            </div>
-            <div className='flex items-center'>
-              <FaCommentDots />
-              <span className='pl-2 text-gray-300 text-sm'>{post._count.comments}</span>
-            </div>
+    <div className='flex h-22 bg-midnight-dark border-[1px] border-slate-700 rounded p-2 w-full cursor-pointer hover:border-main-purple-light transition-colors'>
+      <div className='flex flex-col h-full justify-center border-r-2 border-slate-800 pr-2 w-fit min-w-[5rem]'>
+        <div className='flex items-center mb-1'>
+          <div
+            className='hover:bg-midnight rounded p-1'
+            onClick={handleLike}
+          >
+            {isLiked ? <AiFillLike /> : <AiOutlineLike />}
           </div>
-          <div className='pl-4 flex flex-col w-3/4'>
-            <span className='font-extrabold text-[1em] leading-5 h-fit break-words'>
-              {post.title}
-            </span>
-          </div>
-          <div className='ml-auto flex flex-col'>
-            <span className='text-sm h-fit ml-auto'>
-              <span>by</span>
-              <span className='font-bold underline pl-1 transition-colors'>
-                {post.author.username}
-              </span>
-            </span>
-            <span className='text-xs h-fit mt-auto ml-auto text-gray-500'>
-              Posted on: {moment(post.created_at.toString(), 'YYYY-MM-DD HH:mm:ss').format('lll')}
-            </span>
-            <span className='text-xs h-fit mt-auto text-gray-500'>
-              Last edited:{' '}
-              {moment(post.updated_at?.toString(), 'YYYY-MM-DD HH:mm:ss').format('lll')}
-            </span>
-          </div>
+          <span className='pl-2 text-gray-300 text-sm'>{post._count.likes}</span>
         </div>
-      </a>
-    </Link>
+        <div className='flex items-center mb-1'>
+          <div
+            className='hover:bg-midnight rounded p-1'
+            onClick={handleDislike}
+          >
+            {isDisliked ? <AiFillDislike /> : <AiOutlineDislike />}
+          </div>
+          <span className='pl-2 text-gray-300 text-sm'>{post._count.dislikes}</span>
+        </div>
+        <div className='flex items-center'>
+          <div className='p-1'>
+            <FaCommentDots />
+          </div>
+          <span className='pl-2 text-gray-300 text-sm'>{post._count.comments}</span>
+        </div>
+      </div>
+      <Link
+        href={`/view-post/${post.id}`}
+        passHref
+      >
+        <a className='w-full'>
+          <div className='flex'>
+            <div className='pl-4 flex flex-col w-3/4'>
+              <span className='font-extrabold text-[1em] leading-5 h-fit break-words'>
+                {post.title}
+              </span>
+            </div>
+            <div className='ml-auto flex flex-col'>
+              <span className='text-sm h-fit ml-auto'>
+                <span>by</span>
+                <span className='font-bold underline pl-1 transition-colors'>
+                  {post.author.username}
+                </span>
+              </span>
+              <span className='text-xs h-fit mt-auto ml-auto text-gray-500'>
+                Posted on: {moment(post.created_at.toString(), 'YYYY-MM-DD HH:mm:ss').format('lll')}
+              </span>
+              <span className='text-xs h-fit mt-auto text-gray-500'>
+                Last edited:{' '}
+                {moment(post.updated_at?.toString(), 'YYYY-MM-DD HH:mm:ss').format('lll')}
+              </span>
+            </div>
+          </div>
+        </a>
+      </Link>
+    </div>
   )
 }
