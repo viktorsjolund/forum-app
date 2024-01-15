2aa8ebf97bd309bca6c349fc10feeda42b356e6e
vs222qj <vs222qj@student.lnu.se>
2024-01-15 01:58:32 +0100

    add pagination to user likes

--- src/pages/user/[uname]/liked.tsx
+++ src/pages/user/[uname]/liked.tsx
@@ -1,13 +1,22 @@
 import { Loading } from '@/components/loading'
 import { MinifiedPost } from '@/components/minifiedPost'
+import { PaginationButtons } from '@/components/paginationButtons'
 import { ProfileTemplate } from '@/components/profileTemplate'
 import { trpc } from '@/utils/trpc'
 import { useRouter } from 'next/router'
-import { useEffect } from 'react'
+import { useEffect, useMemo } from 'react'
+const POST_LIMIT = 2 as const
 
 const Liked = () => {
   const router = useRouter()
-  const { uname } = router.query as { uname: string }
+  const { uname, page } = router.query as { uname: string; page?: string }
+  let pageNr = page ? parseInt(page) : 1
+  if (isNaN(pageNr)) {
+    pageNr = 1
+  } else if (pageNr < 1) {
+    pageNr = 1
+  }
+
   const { data: me } = trpc.useQuery(['user.me'])
   const {
     data: user,
@@ -17,14 +26,25 @@ const Liked = () => {
   const {
     data: posts,
     isLoading: isPostsLoading,
-    refetch
-  } = trpc.useQuery(['post.byUserLikes', { userId: user?.id! }], { enabled: false })
+    refetch: refetchLikes
+  } = trpc.useQuery(
+    ['post.byUserLikes', { userId: user?.id!, skip: (pageNr - 1) * POST_LIMIT, take: POST_LIMIT }],
+    { enabled: false }
+  )
+  const { data: postCount, refetch: refetchCount } = trpc.useQuery(
+    ['post.countByUserLikes', { userId: user?.id! }],
+    {
+      enabled: false
+    }
+  )
+  const numberOfPages = useMemo(() => Math.ceil((postCount || 1) / POST_LIMIT), [postCount])
 
   useEffect(() => {
     if (user) {
-      refetch()
+      refetchLikes()
+      refetchCount()
     }
-  }, [user, refetch])
+  }, [user, refetchLikes, refetchCount])
 
   if (isUserLoading || isPostsLoading) {
     return <Loading />
@@ -34,18 +54,32 @@ const Liked = () => {
     return <span>User not found.</span>
   }
 
+  const handleNewPage = () => {
+    refetchLikes()
+  }
+
   return (
     <ProfileTemplate
       me={me}
       username={uname}
     >
-      <div className='flex flex-col'>
-        {posts?.map((post) => (
-          <MinifiedPost
-            key={post.id}
-            post={post}
+      <div className='p-5'>
+        {postCount && (
+          <PaginationButtons
+            handleNewPage={handleNewPage}
+            numberOfPages={numberOfPages}
+            pageNr={pageNr}
+            postCount={postCount}
           />
-        ))}
+        )}
+        <div className='flex flex-col'>
+          {posts?.map((post) => (
+            <MinifiedPost
+              key={post.id}
+              post={post}
+            />
+          ))}
+        </div>
       </div>
     </ProfileTemplate>
   )
