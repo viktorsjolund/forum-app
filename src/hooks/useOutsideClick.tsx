482bb6124fdd8ad3e56f1bb098abc80e8a5ed1be
vs222qj <vs222qj@student.lnu.se>
2024-01-24 21:05:13 +0100

    add ref array option to useoutsideclick hook

--- src/hooks/useOutsideClick.tsx
+++ src/hooks/useOutsideClick.tsx
@@ -1,16 +1,26 @@
 import { useEffect, useRef } from 'react'
 
-export function useOutsideClick<T1 extends HTMLElement, T2 extends HTMLElement = any>(callback: () => void) {
+export function useOutsideClick<
+  T1 extends HTMLElement,
+  T2 extends HTMLElement = any,
+  T3 extends HTMLElement = any
+>(callback: () => void) {
   const ref = useRef<T1>(null)
   const triggerRef = useRef<T2>(null)
+  const refArray = useRef<Array<T3 | null>>([])
 
   useEffect(() => {
     const handleClick = (e: MouseEvent) => {
-      if (!ref.current || !triggerRef.current) return
-      if (e.target instanceof Node && !ref.current.contains(e.target) && !triggerRef.current.contains(e.target)) {
-        callback()
+      if (e.target instanceof Node) {
+        if (
+          !refArray.current.includes(e.target as T3) &&
+          !ref.current?.contains(e.target) &&
+          !triggerRef.current?.contains(e.target)
+        ) {
+          callback()
+        }
       }
-   }
+    }
     document.addEventListener('click', handleClick)
 
     return () => {
@@ -20,6 +30,7 @@ export function useOutsideClick<T1 extends HTMLElement, T2 extends HTMLElement =
 
   return {
     ref,
-    triggerRef
+    triggerRef,
+    refArray
   }
 }
