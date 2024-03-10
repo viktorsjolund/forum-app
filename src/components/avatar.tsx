6925e1be9f63b1cfa5a91207eb80522cfaec6ca5
vs222qj <vs222qj@student.lnu.se>
2024-03-10 17:25:09 +0100

    add optional width and height props

--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -2,24 +2,24 @@ import Link from 'next/link'
 import Image from 'next/image'
 
 type AvatarProps = {
-  username?: string | null
+  username: string
   src?: string | null
+  width?: number
+  height?: number
 }
 
 export const Avatar = (props: AvatarProps) => {
-  const { username, src } = props
+  const { username, src, width, height } = props
 
   return (
-    <>
-      <Link href={`/user/${username}`}>
-        <Image
-          src={src ? src : '/images/avatar.png'}
-          alt='avatar'
-          width={40}
-          height={40}
-          className='rounded-full cursor-pointer'
-        />
-      </Link>
-    </>
+    <Link href={`/user/${encodeURIComponent(username)}`}>
+      <Image
+        src={src ? src : '/images/avatar.png'}
+        alt='avatar'
+        width={width || 40}
+        height={height || 40}
+        className='rounded-full cursor-pointer'
+      />
+    </Link>
   )
 }
